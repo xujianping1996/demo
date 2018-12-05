c8864697b50c264be657d0d447367612fb4eadaa
xujianping <jianp.xu.@sunyard.com>
2018-12-05 09:35:24 +0800

    添加log4j2

--- src/main/java/com/jianping/demo/controller/LoginController.java
+++ src/main/java/com/jianping/demo/controller/LoginController.java
@@ -2,15 +2,18 @@
 
 
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.jianping.demo.entity.User;
 import com.jianping.demo.service.LoginService;
 
 @Controller
@@ -18,9 +21,21 @@ import com.jianping.demo.service.LoginService;
 public class LoginController {
 	@Autowired
 	private LoginService loginService;
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	@RequestMapping(value="/handler")
-	public ModelAndView handler(ModelAndView mav) {
+	public ModelAndView handler(ModelAndView mav,HttpServletRequest request) {
 		mav.setViewName("login");
+		String ip = request.getHeader("x-forwarded-for"); 
+		if (ip == null || ip.length() == 0 ||"unknown".equalsIgnoreCase(ip)) { 
+		ip = request.getHeader("Proxy-Client-IP"); 
+		} 
+		if (ip == null || ip.length() == 0 ||"unknow".equalsIgnoreCase(ip)) { 
+		ip = request.getHeader("WL-Proxy-Client-IP"); 
+		} 
+		if (ip == null || ip.length() == 0 ||"unknown".equalsIgnoreCase(ip)) { 
+		ip = request.getRemoteAddr(); 
+		} 
+		logger.info("客户端IP:"+ip);
 		return mav;
 	}
 	@RequestMapping(value = "/verification")
